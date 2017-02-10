fd822394a8bd890d0771223df95e267ee530fee8
shanbo.xuan <shanbo.xuan@mi-me.com>
2017-02-10 10:31:49 +0800

    react

--- src/myapp/containers/index.js
+++ src/myapp/containers/index.js
@@ -8,21 +8,32 @@ import thunk from 'redux-thunk';
 import App from './app';
 import servers from './servert'
 import taps from './tap'
-import Aremover from './myadd'
+
+
+
+const Aremover = (location, cb) => {  //路由按需处理  示列
+	require.ensure([], require => {
+	    cb(null, require('./myadd').default)
+	},'Aremover')
+};
+
+
 
 let enhancer = compose(
         applyMiddleware(thunk, createLogger()),
         window.devToolsExtension ? window.devToolsExtension() : f => f
 );
 
+
 const stores = createStore(servers,enhancer);
+
 
 render(
 	<Provider store={stores}>
 		<Router history={hashHistory}>
 	      <Route path="/" component={App} />
 	      <Route path="tapgo" component={taps} />
-	      <Route path="myadds" component={Aremover} />
+	      <Route path="myadds" getComponent={Aremover} />
 	    </Router>
 	</Provider>,
 	document.getElementById('root')
